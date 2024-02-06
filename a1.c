3aa2e6af94ff278ac179f5b0ca138d8863cfd21b
Wayne Chen <wchen206@my.bcit.ca>
2024-02-06 11:06:55 -0800

    added tolowercase method

--- a1.c
+++ a1.c
@@ -3,33 +3,39 @@
 #include <string.h>
 
 int checkAnagram(char str[], char str2[]) {
+  toSmallCase(str);
+  toSmallCase(str2);
+
+  int length1 = strlen(str);
+  int length2 = strlen(str2);
 
 
 
+  for (int i = 0; i < length1; i++) {
+    for (int j = 0; j < length2; j++) {
+      if (str[i] == str[j]) {
+        
+      }
+    }
+  }
 }
 
 void toSmallCase(char *str) {
   int length = strlen(str);
 
   for (int i = 0; i < length; i++) {
-    
+    str[i] = toLower(str[i]);
   }
 }
 
 int main(int argc, char *argv[]) {
-  if (argc != 4) {
-      fprintf(stderr, "Usage: %s <input_file> <output_file> <shift>\n", argv[0]);
+  if (argc != 3) {
+      fprintf(stderr, "Usage: %s <input_file> <output_file>\n", argv[0]);
       return 1;
   }
 
   const char *input_path = argv[1];
   const char *output_path = argv[2];
-  int shift = atoi(argv[3]);
-
-  if (shift < 0 || shift >= 26) {
-      perror("Shift must be between 0 and 25 (inclusive)\n");
-      return 1;
-  }
 
   FILE *input_file = fopen(input_path, "r");
   FILE *output_file = fopen(output_path, "w");
